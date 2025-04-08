07701a5257b2e0304b09202333f91ce7deb4a5c4
Cody Gunton <codygunton@gmail.com>
2025-04-08 00:35:53 -0400

    this does... something (28s)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 pub fn main() {
     let target_dir = "/tmp/jolt-guest-targets";
-    let program = guest::compile_fib(target_dir);
+    let program = guest::compile_blake2s_hash(target_dir);
 
-    let prover_preprocessing = guest::preprocess_prover_fib(&program);
-    let verifier_preprocessing = guest::preprocess_verifier_fib(&program);
+    let prover_preprocessing = guest::preprocess_prover_blake2s_hash(&program);
+    let verifier_preprocessing = guest::preprocess_verifier_blake2s_hash(&program);
 
-    let prove_fib = guest::build_prover_fib(program, prover_preprocessing);
-    let verify_fib = guest::build_verifier_fib(verifier_preprocessing);
+    let prove_blake2s_hash = guest::build_prover_blake2s_hash(program, prover_preprocessing);
+    let verify_blake2s_hash = guest::build_verifier_blake2s_hash(verifier_preprocessing);
 
-    let (output, proof) = prove_fib(50);
-    let is_valid = verify_fib(50, output, proof);
+    let (output, proof) = prove_blake2s_hash(b"Hello, world!");
+    let is_valid = verify_blake2s_hash(b"Hello, world!", output, proof);
 
-    println!("output: {}", output);
+    println!("output: {:?}", output);
     println!("valid: {}", is_valid);
 }
